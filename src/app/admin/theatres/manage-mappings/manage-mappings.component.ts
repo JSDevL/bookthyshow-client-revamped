feba350250e14301cf0ea92bd5956a4c52dfd76b
arfath.7920 <arfath.7920@gmail.com>
2017-06-05 17:45:34 +0530

    Finished manage-mappings

--- src/app/admin/theatres/manage-mappings/manage-mappings.component.ts
+++ src/app/admin/theatres/manage-mappings/manage-mappings.component.ts
@@ -1,10 +1,12 @@
-import {Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges} from '@angular/core';
+import {Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges, ViewChild} from '@angular/core';
 import {Theatre} from '../../../shared/models/theatre.model';
 import {Movie} from '../../../shared/models/movie.model';
 import {Mapping} from 'app/shared/models/mapping.model';
 import {Subscription} from 'rxjs/Subscription';
 import {MappingsService} from '../../../shared/services/mappings.service';
 import * as _ from 'underscore';
+import {NgForm} from '@angular/forms';
+import {Response} from '@angular/http';
 
 @Component({
     selector: 'app-manage-mappings',
@@ -12,6 +14,12 @@ import * as _ from 'underscore';
     styleUrls: ['./manage-mappings.component.scss']
 })
 export class ManageMappingsComponent implements OnInit, OnDestroy, OnChanges {
+
+    @ViewChild('newDateForm')
+    newDateForm: NgForm;
+
+    @ViewChild('newTimingForm')
+    newTimingForm: NgForm;
 
     @Input('selectedTheatre')
     selectedTheatre: Theatre;
@@ -40,7 +48,11 @@ export class ManageMappingsComponent implements OnInit, OnDestroy, OnChanges {
 
     private searchActiveMapping() {
         this.activeMapping = _.find(this.mappings, (mapping: Mapping) => {
-            return _.isEqual(mapping.theatre, this.selectedTheatre) && _.isEqual(mapping.movie, this.selectedMovie);
+            if (this.selectedTheatre) {
+                return mapping.theatre._id === this.selectedTheatre._id && mapping.movie._id === this.selectedMovie._id;
+            } else {
+                return undefined;
+            }
         });
     }
 
@@ -54,6 +66,56 @@ export class ManageMappingsComponent implements OnInit, OnDestroy, OnChanges {
                 dates: []
             }
         ]);
+    }
+
+    public addDate() {
+        this.activeMapping.dates.push(new Date(this.newDateForm.value['date']));
+        this.activeMapping.dates = _.sortBy(_.uniq(this.activeMapping.dates));
+
+        this.newDateForm.reset();
+    }
+
+    public addTiming() {
+        const hours: number = parseInt(this.newTimingForm.value['timing'].split(':')[0], 10);
+        const minutes: number = parseInt(this.newTimingForm.value['timing'].split(':')[1], 10);
+        const dateObject: Date = new Date();
+        dateObject.setHours(hours);
+        dateObject.setMinutes(minutes);
+        this.activeMapping.timings.push(dateObject);
+        this.activeMapping.timings = _.sortBy(_.uniq(this.activeMapping.timings));
+
+        this.newTimingForm.reset();
+    }
+
+    public deleteDate(dateToDelete) {
+        this.activeMapping.dates = _.reject(this.activeMapping.dates, (date: Date) => date === dateToDelete);
+    }
+
+    public deleteTiming(timingToDelete) {
+        this.activeMapping.timings = _.reject(this.activeMapping.timings, (timing: Date) => timing === timingToDelete);
+    }
+
+    public saveMapping() {
+        if (!this.activeMapping._id) {
+            this.mappingsService.postMapping(this.activeMapping).subscribe((mapping: Mapping) => {
+                this.activeMapping = _.extend(this.activeMapping, mapping);
+                alert('Mapping saved.');
+            });
+        } else {
+            this.mappingsService.putMapping(this.activeMapping).subscribe((mapping: Mapping) => {
+                this.activeMapping = _.extend(this.activeMapping, mapping);
+                alert('Mapping saved.');
+            });
+        }
+    }
+
+    public deleteMapping() {
+        this.mappingsService.deleteMapping(this.activeMapping).subscribe((response: Response) => {
+            this.mappingsService.mappingsSource.next(
+                _.reject(this.mappings, (mapping: Mapping) => mapping._id === this.activeMapping._id)
+            );
+            alert('Mapping deleted.');
+        });
     }
 
     ngOnDestroy() {
